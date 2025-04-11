8f4970efc9b95b24759f5d79e56dcffa65690c2f
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-04-11 12:27:59 +1000

    FormFieldList

---
+++ src/test/java/walkingkooka/validation/form/FormFieldListTest.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.validation.form;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.collect.list.ImmutableListTesting;
+import walkingkooka.net.Url;
+import walkingkooka.net.email.EmailAddress;
+import walkingkooka.reflect.ClassTesting;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.text.printer.TreePrintableTesting;
+import walkingkooka.tree.json.JsonNode;
+import walkingkooka.tree.json.marshall.JsonNodeMarshallingTesting;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
+import walkingkooka.validation.TestValidationReference;
+import walkingkooka.validation.ValidationValueTypeName;
+import walkingkooka.validation.provider.ValidatorSelector;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public final class FormFieldListTest implements ImmutableListTesting<FormFieldList<TestValidationReference>, FormField<TestValidationReference>>,
+    TreePrintableTesting,
+    ClassTesting<FormFieldList<TestValidationReference>>,
+    JsonNodeMarshallingTesting<FormFieldList<TestValidationReference>> {
+
+    @Test
+    public void testSetElementsDoesntDoubleWrap() {
+        final FormFieldList<TestValidationReference> list = this.createList();
+
+        assertSame(
+            list,
+            list.setElements(list)
+        );
+    }
+
+    @Override
+    public FormFieldList<TestValidationReference> createList() {
+        return FormFieldList.<TestValidationReference>empty()
+            .concat(
+                FormField.with(
+                        new TestValidationReference("Hello")
+                    ).setLabel("Label111")
+                    .setType(
+                        Optional.of(
+                            ValidationValueTypeName.with("Type222")
+                        )
+                    ).setValue(
+                        Optional.of("Value333")
+                    ).setValidator(
+                        Optional.of(
+                            ValidatorSelector.parse("Validator4444")
+                        )
+                    )
+            );
+    }
+
+    // Json.............................................................................................................
+
+    @Test
+    public void testMarshall() {
+        this.marshallAndCheck(
+            this.createJsonNodeMarshallingValue(),
+            "[\n" +
+                "  {\n" +
+                "    \"reference\": {\n" +
+                "      \"type\": \"test-validation-reference\",\n" +
+                "      \"value\": \"Hello\"\n" +
+                "    },\n" +
+                "    \"label\": \"Label111\",\n" +
+                "    \"type\": \"Type222\",\n" +
+                "    \"value\": \"Value333\",\n" +
+                "    \"validator\": \"Validator4444\"\n" +
+                "  }\n" +
+                "]"
+        );
+    }
+
+    @Test
+    public void testUnmarshall() {
+        this.unmarshallAndCheck(
+            "[\n" +
+                "  {\n" +
+                "    \"reference\": {\n" +
+                "      \"type\": \"test-validation-reference\",\n" +
+                "      \"value\": \"Hello\"\n" +
+                "    },\n" +
+                "    \"label\": \"Label111\",\n" +
+                "    \"type\": \"Type222\",\n" +
+                "    \"value\": \"Value333\",\n" +
+                "    \"validator\": \"Validator4444\"\n" +
+                "  }\n" +
+                "]",
+            this.createJsonNodeMarshallingValue()
+        );
+    }
+
+    @Test
+    public void testMarshallMixedFormFields() {
+        this.marshallAndCheck(
+            FormFieldList.<TestValidationReference>empty()
+                .concat(
+                    FormField.with(
+                        new TestValidationReference("Hello1")
+                    ).setValue(
+                        Optional.of(
+                            EmailAddress.parse("hello1@example.com")
+                        )
+                    )
+                ).concat(
+                    FormField.with(
+                        new TestValidationReference("Hello2")
+                    ).setLabel("Label2"
+                    ).setType(
+                        Optional.of(
+                            ValidationValueTypeName.with("Type222")
+                        )
+                    ).setValue(
+                        Optional.of(
+                            Url.parse("https://example.com/2")
+                        )
+                    )
+                ),
+            "[\n" +
+                "  {\n" +
+                "    \"reference\": {\n" +
+                "      \"type\": \"test-validation-reference\",\n" +
+                "      \"value\": \"Hello1\"\n" +
+                "    },\n" +
+                "    \"value\": {\n" +
+                "      \"type\": \"email-address\",\n" +
+                "      \"value\": \"hello1@example.com\"\n" +
+                "    }\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"reference\": {\n" +
+                "      \"type\": \"test-validation-reference\",\n" +
+                "      \"value\": \"Hello2\"\n" +
+                "    },\n" +
+                "    \"label\": \"Label2\",\n" +
+                "    \"type\": \"Type222\",\n" +
+                "    \"value\": {\n" +
+                "      \"type\": \"url\",\n" +
+                "      \"value\": \"https://example.com/2\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "]"
+        );
+    }
+
+    @Override
+    public FormFieldList<TestValidationReference> unmarshall(final JsonNode json,
+                                                             final JsonNodeUnmarshallContext context) {
+        return FormFieldList.unmarshall(
+            json,
+            context
+        );
+    }
+
+    @Override
+    public FormFieldList<TestValidationReference> createJsonNodeMarshallingValue() {
+        return this.createList();
+    }
+
+    // ClassTesting.....................................................................................................
+
+    @Override
+    public Class<FormFieldList<TestValidationReference>> type() {
+        return Cast.to(FormFieldList.class);
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
