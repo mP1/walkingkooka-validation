9a1a6de236d3de341eb570ffb06a0bd7172d8ea5
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-04-28 10:55:07 +1000

    FakeFormHandlerExpressionEvaluationContext

---
+++ src/main/java/walkingkooka/validation/form/function/FakeFormHandlerExpressionEvaluationContext.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.validation.form.function;
+
+import walkingkooka.environment.EnvironmentValueName;
+import walkingkooka.net.email.EmailAddress;
+import walkingkooka.tree.expression.ExpressionReference;
+import walkingkooka.tree.expression.FakeExpressionEvaluationContext;
+import walkingkooka.validation.ValidationReference;
+import walkingkooka.validation.ValidatorContext;
+import walkingkooka.validation.form.Form;
+import walkingkooka.validation.form.FormField;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+
+public class FakeFormHandlerExpressionEvaluationContext<R extends ValidationReference, S> extends FakeExpressionEvaluationContext implements FormHandlerExpressionEvaluationContext<R, S> {
+
+    public FakeFormHandlerExpressionEvaluationContext() {
+        super();
+    }
+
+    @Override
+    public FormHandlerExpressionEvaluationContext<R, S> enterScope(final Function<ExpressionReference, Optional<Optional<Object>>> scoped) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Form<R> form() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Comparator<R> formFieldReferenceComparator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ValidatorContext<R> validatorContext(final R reference) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<?> loadFieldValue(final R reference) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public S saveFieldValues(final List<FormField<R>> formFields) {
+        throw new UnsupportedOperationException();
+    }
+
+    // EnvironmentContext...............................................................................................
+
+    @Override
+    public <T> Optional<T> environmentValue(final EnvironmentValueName<T> environmentValueName) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Set<EnvironmentValueName<?>> environmentValueNames() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<EmailAddress> user() {
+        throw new UnsupportedOperationException();
+    }
+}
