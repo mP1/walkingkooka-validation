fad49b9cc28196089b35905714a746f755f685d8
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-06-01 12:30:06 +1000

    TextToValidationValueTypeNameConverter
    - Closes https://github.com/mP1/walkingkooka-validation/issues/225
    - Converter Text -> ValidationValueTypeName

---
+++ src/test/java/walkingkooka/validation/convert/TextToValidationValueTypeNameConverterTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.validation.convert;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.Either;
+import walkingkooka.convert.Converter;
+import walkingkooka.convert.ConverterTesting2;
+import walkingkooka.convert.Converters;
+import walkingkooka.convert.FakeConverterContext;
+import walkingkooka.validation.ValidationValueTypeName;
+
+public final class TextToValidationValueTypeNameConverterTest implements ConverterTesting2<TextToValidationValueTypeNameConverter<FakeConverterContext>, FakeConverterContext> {
+
+    @Test
+    public void testConvertNumberToValidationValueTypeNameConverter() {
+        this.convertFails(
+            123,
+            ValidationValueTypeName.class
+        );
+    }
+
+    @Test
+    public void testConvertStringToValidationValueTypeNameConverter() {
+        final String text = "text";
+
+        this.convertAndCheck(
+            text,
+            ValidationValueTypeName.with(text)
+        );
+    }
+
+    @Override
+    public TextToValidationValueTypeNameConverter<FakeConverterContext> createConverter() {
+        return TextToValidationValueTypeNameConverter.instance();
+    }
+
+    @Override
+    public FakeConverterContext createContext() {
+        return new FakeConverterContext() {
+            @Override
+            public boolean canConvert(final Object value,
+                                      final Class<?> type) {
+                return this.textTo.canConvert(
+                    value,
+                    type,
+                    this
+                );
+            }
+
+            @Override
+            public <T> Either<T, String> convert(final Object value,
+                                                 final Class<T> target) {
+                return this.textTo.convert(
+                    value,
+                    target,
+                    this
+                );
+            }
+
+            private final Converter<FakeConverterContext> textTo = Converters.characterOrCharSequenceOrHasTextOrStringToCharacterOrCharSequenceOrString();
+        };
+    }
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+            TextToValidationValueTypeNameConverter.instance(),
+            "Text to ValidationValueTypeName"
+        );
+    }
+
+    @Override
+    public Class<TextToValidationValueTypeNameConverter<FakeConverterContext>> type() {
+        return Cast.to(TextToValidationValueTypeNameConverter.class);
+    }
+}
