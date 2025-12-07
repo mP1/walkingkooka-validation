a82ac813fe53597d1efcf5cde5eb21d9b944a7fe
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-12-07 14:25:21 +1100

    https://github.com/mP1/walkingkooka-environment/pull/183 EnvironmentContextDelegator.setLocale/setUser
    - https://github.com/mP1/walkingkooka-environment/pull/183
    - EnvironmentContextDelegator.setLocale/setUser

--- src/main/java/walkingkooka/validation/ValidatorContextDelegator.java
+++ src/main/java/walkingkooka/validation/ValidatorContextDelegator.java
@@ -22,9 +22,13 @@ import walkingkooka.convert.CanConvertDelegator;
 import walkingkooka.environment.EnvironmentContext;
 import walkingkooka.environment.EnvironmentContextDelegator;
 import walkingkooka.environment.EnvironmentValueName;
+import walkingkooka.net.email.EmailAddress;
 import walkingkooka.text.LineEnding;
 import walkingkooka.tree.expression.ExpressionEvaluationContext;
 import walkingkooka.validation.provider.ValidatorSelector;
+
+import java.util.Locale;
+import java.util.Optional;
 
 public interface ValidatorContextDelegator<T extends ValidationReference> extends ValidatorContext<T>,
     CanConvertDelegator,
@@ -86,6 +90,20 @@ public interface ValidatorContextDelegator<T extends ValidationReference> extend
         return this;
     }
 
+    @Override
+    default ValidatorContext<T> setLocale(final Locale locale) {
+        this.environmentContext()
+            .setLocale(locale);
+        return this;
+    }
+
+    @Override
+    default ValidatorContext<T> setUser(final Optional<EmailAddress> user) {
+        this.environmentContext()
+            .setUser(user);
+        return this;
+    }
+    
     @Override
     default EnvironmentContext environmentContext() {
         return this.validatorContext();
